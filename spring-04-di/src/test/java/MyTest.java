a0e94df3039919546c7bcb1f3176697b4c6418ae
lirulei <lirulei@baixing.com>
2021-01-17 13:20:11 +0800

    commit

---
+++ spring-04-di/src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import com.kuang.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
+        System.out.println(student.getWife());
+    }
+}
