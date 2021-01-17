43aa88402fc2b14b7c9308122d1b07bb2aa7473f
lirulei <lirulei@baixing.com>
2021-01-17 14:13:17 +0800

    commit

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -9,5 +9,27 @@ public class MyTest {
 //        System.out.println(student.getName());
 //        System.out.println(student.getWife());
         System.out.println(student.toString());
+
+
+        /***  输出的结果如下：
+         * Student{
+         *  name='Lee二狗',
+         *  address=Address{address='上海市闵行区'},
+         *  books=[红楼梦, 西游记, 水浒传, 三国演义],
+         *  hobbys=[健身, 游泳, 听歌],
+         *  card={身份证=11111111111, 银行卡=22222222222},
+         *  games=[FIFA2020, 使命召唤, 战地2],
+         *  wife='null',
+         *  info={班级=0502, 学号=13141516, url=www.qq.com, username=leeergou}
+         *  }
+         */
+
+
+
+
+
+
+
+
     }
 }
